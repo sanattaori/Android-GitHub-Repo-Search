8ccb9274d26ce24fbe78a51061406b7603b610dc
sanattaori <sanattaori3@gmail.com>
2018-02-16 23:14:26 +0530

    networking added

--- app/src/main/java/com/speedev/sanat/sunshine/MainActivity.java
+++ app/src/main/java/com/speedev/sanat/sunshine/MainActivity.java
@@ -1,8 +1,16 @@
 package com.speedev.sanat.sunshine;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.speedev.sanat.sunshine.data.SunshinePreferences;
+import com.speedev.sanat.sunshine.utilities.NetworkUtils;
+import com.speedev.sanat.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +23,67 @@ public class MainActivity extends AppCompatActivity {
 
         mWeatherTextView = findViewById(R.id.weather_data_text_view);
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-        };
+//        String[] dummyWeatherData = {
+//                "Today, May 17 - Clear - 17°C / 15°C",
+//                "Tomorrow - Cloudy - 19°C / 15°C",
+//                "Thursday - Rainy- 30°C / 11°C",
+//        };
+//
+//        for (String s : dummyWeatherData) {
+//            mWeatherTextView.append(s+"\n\n\n");
+//        }
+        loadWeatherData();
+//
+    }
 
-        for (String s : dummyWeatherData) {
-            mWeatherTextView.append(s+"\n\n\n");
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        // Override the doInBackground method to perform your network requests
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            /* If there's no zip code, there's nothing to look up. */
+            if (params.length == 0) {
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                //inline variable
+                return OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         }
-        //layouts display
+
+        // Override the onPostExecute method to display the results of the network request
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            if (weatherData != null) {
+                /*
+                 * Iterate through the array and append the Strings to the TextView. The reason why we add
+                 * the "\n\n\n" after the String is to give visual separation between each String in the
+                 * TextView. Later, we'll learn about a better way to display lists of data.
+                 */
+                for (String weatherString : weatherData) {
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                }
+            }
+        }
     }
 }
