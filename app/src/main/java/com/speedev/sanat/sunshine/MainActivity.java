eb403fb8257cb282bcf05dde0cb172fec352ddb4
sanattaori <sanattaori3@gmail.com>
2018-02-16 22:06:07 +0530

    layouts done

--- app/src/main/java/com/speedev/sanat/sunshine/MainActivity.java
+++ app/src/main/java/com/speedev/sanat/sunshine/MainActivity.java
@@ -2,12 +2,28 @@ package com.speedev.sanat.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWeatherTextView = findViewById(R.id.weather_data_text_view);
+
+        String[] dummyWeatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+        };
+
+        for (String s : dummyWeatherData) {
+            mWeatherTextView.append(s+"\n\n\n");
+        }
+        //layouts display
     }
 }
